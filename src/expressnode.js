02c148285f733cf8c7165b84b98990252e058c4d
Wille2 <villeahti@hotmail.com>
2013-11-14 20:28:23 +0200

    added parameters to rest service

--- src/expressnode.js
+++ src/expressnode.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var app = express();
+var counter = 0;
+
+var fs = require('fs')
 
 app.get('/', function(req, res) {
-	res.type('text/plain'); // set content-type
-	res.send('i am a beautiful butterfly'); // send text response
+	res.type('text/json'); // set content-type
+	fs.readFile(req.query.restaurant + '.json', 'utf8', function(err, data) {
+		if (err) {
+			console.log(err);
+			res.type('text/plain');
+			res.send("ERROR 404: file not found")
+		}
+		res.json(data);
+	});
+	console.log('answered to request #' + ++counter);
 });
 
 app.listen(process.env.PORT || 4730);
