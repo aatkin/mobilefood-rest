55f7ac75abec79db360c781f00e43a0ef62c7195
Wille2 <villeahti@hotmail.com>
2013-11-21 20:07:31 +0200

    improved rest service

--- src/expressnode.js
+++ src/expressnode.js
@@ -3,17 +3,23 @@ var app = express();
 var counter = 0;
 
 var fs = require('fs')
+var DIR = JSON.parse(fs.readFileSync('config.json')).dir;
+console.log("Current output dir: " + DIR);
 
 app.get('/', function(req, res) {
 	res.type('text/json'); // set content-type
-	fs.readFile(req.query.restaurant + '.json', 'utf8', function(err, data) {
+	var restaurant = req.query.restaurant;
+	var year = req.query.year;
+	var week = req.query.week;
+	fs.readFile(DIR + year + "_w" + week + "_" + restaurant + '.json', 'utf8', function(err, data) {
 		if (err) {
 			console.log(err);
 			res.type('text/plain');
-			res.send("ERROR 404: file not found")
+			res.send("ERROR 404: file not found");
 		}
-		res.json(data);
+		res.send(data);
 	});
+	console.log("Client queried for: " + JSON.stringify(req.query));
 	console.log('answered to request #' + ++counter);
 });
 
